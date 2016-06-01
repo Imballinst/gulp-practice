8afad01695e3f6273c1992a2ed625d3f74414b3e
Imballinst <lucent_rays@yahoo.com>
2016-06-01 16:22:31 +0700

    fix issue #1

--- dev/assets/js/load.js
+++ dev/assets/js/load.js
@@ -1,22 +1,24 @@
-/*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
-(function( w ){
-  var loadJS = function( src, cb ){
-    "use strict";
-    var ref = w.document.getElementsByTagName( "script" )[ 0 ];
-    var script = w.document.createElement( "script" );
-    script.src = src;
-    script.async = true;
-    ref.parentNode.insertBefore( script, ref );
-    if (cb && typeof(cb) === "function") {
-      script.onload = cb;
-    }
-    return script;
-  };
-  // commonjs
-  if( typeof module !== "undefined" ){
-    module.exports = loadJS;
+$.getJSON("file.json", function(json) {
+  document.getElementById("header").innerHTML = json.header;
+  document.getElementById("footer").innerHTML = json.footer;
+
+  // DOM Element
+  var row = $('<div class="row"></div>');
+  var col = $('<div class="col-md-12"></div>');
+  row.append(col);
+
+  for (i = 0; i < json.articles.length; i++) {
+    // Empty Content
+    var content = $('<div class="content"></div>');
+    var title = $('<div class="title"></div>');
+    var content_image = $('<div class="content-image"></div>');
+    var text = $('<p class="base-text"></div>');
+    var imageSection = '<img src="' + json.articles[i].image + '">';
+
+    title.append(json.articles[i].title);
+    content_image.append(imageSection);
+    text.append(json.articles[i].text);
+
+    $(".container").append(content.append(title).append(content_image).append(content).append(text));
   }
-  else {
-    w.loadJS = loadJS;
-  }
-}( typeof global !== "undefined" ? global : this ));
+});
