c1c18bcaaee7ebb733d3cc5bd7a2cb21a99e96de
Imballinst <lucent_rays@yahoo.com>
2016-06-02 09:55:22 +0700

    added row and column div before content

--- dev/assets/js/load.js
+++ dev/assets/js/load.js
@@ -2,23 +2,27 @@ $.getJSON("file.json", function(json) {
   document.getElementById("header").innerHTML = json.header;
   document.getElementById("footer").innerHTML = json.footer;
 
-  // DOM Element
-  var row = $('<div class="row"></div>');
-  var col = $('<div class="col-md-12"></div>');
-  row.append(col);
-
-  for (i = 0; i < json.articles.length; i++) {
-    // Empty Content
+  // For each article inside articles array in the JSON
+  json.articles.forEach(function(article) {
+    // DOM element
+    var row = $('<div class="row"></div>');
+    var col = $('<div class="col-md-12"></div>');
     var content = $('<div class="content"></div>');
     var title = $('<div class="title"></div>');
-    var content_image = $('<div class="content-image"></div>');
+    var image = $('<div class="content-image"></div>');
     var text = $('<p class="base-text"></div>');
-    var imageSection = '<img src="' + json.articles[i].image + '">';
+    // Image Element
+    var imageElement = '<img src="' + article.image + '">';
 
-    title.append(json.articles[i].title);
-    content_image.append(imageSection);
-    text.append(json.articles[i].text);
+    title.append(article.title);
+    image.append(imageElement);
+    text.append(article.text);
 
-    $(".container").append(content.append(title).append(content_image).append(content).append(text));
-  }
+    $(".container").append(row
+      .append(col
+        .append(content
+          .append(title)
+          .append(image)
+          .append(text))));
+  });
 });
